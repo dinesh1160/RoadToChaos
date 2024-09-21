567c0c27eac7f15b0d62014022d48d49a98f5341
dinesh1160 <dinesharunachalam116@gmail.com>
2024-09-21 11:27:28 +0530

    Car mechanics -implemented

--- src/Car/car.gd
+++ src/Car/car.gd
@@ -1,69 +1,63 @@
 extends CharacterBody2D
 
+var wheel_base = 60  # Distance from front to rear wheel
+var steering_angle = 15  # Amount that front wheel turns, in degrees
+var engine_power = 1000  # Forward acceleration force.
+var friction = -55
+var drag = -0.06
+var braking = -450
+#var max_speed_reverse = 300
+var slip_speed = 400  # Speed where traction is reduced
+var traction_fast = 2.0 # High-speed traction
+var traction_slow = 10  # Low-speed traction
 
-var wheel_base = 70 #separates the front and back wheel
-var steering_angle = 15
-var steer_direction
-var engine_power = 800
-var braking = -200
 var acceleration = Vector2.ZERO
-var traction_slow = 0.7
-var traction_fast = 0.1
-var slip_speed = 400
-var max_speed_reversed = 250
-var friction = -0.9
-var drag = -0.0001
+var steer_direction
 
 func _physics_process(delta):
 	acceleration = Vector2.ZERO
 	get_input()
-	apply_friction()
+	apply_friction(delta)
 	calculate_steering(delta)
-	
 	velocity += acceleration * delta
 	move_and_slide()
-	
+
 	
 func get_input():
-	var turn = 0
-	if Input.is_action_pressed("right"):
-		turn+=1
-	if Input.is_action_pressed("left"):
-		turn-=1
-	steer_direction = turn*deg_to_rad(steering_angle)
-	
+	var turn = Input.get_axis("left", "right")
+	steer_direction = turn * deg_to_rad(steering_angle)
 	if Input.is_action_pressed("front"):
-		acceleration = transform.x*engine_power
-	
+		acceleration = transform.x * engine_power
 	if Input.is_action_pressed("back"):
-		acceleration = transform.x*braking
+		acceleration = transform.x * braking
 	
-func calculate_steering(delta): 
-	#it implies the actual car steering mechanic where only the front wheels changes direction
-	var rear_wheel = position - transform.x * wheel_base/2.0
-	var front_wheel = position + transform.x * wheel_base/2.0
+		
+func apply_friction(delta):
+	if acceleration == Vector2.ZERO and velocity.length() < 50:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction * delta
+	var drag_force = velocity * velocity.length() * drag * delta
+	acceleration += drag_force + friction_force
+
+
+func calculate_steering(delta):
+	# 1. Find the wheel positions
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	# 2. Move the wheels forward
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
-
-
+	# 3. Find the new direction vector
 	var new_heading = (front_wheel - rear_wheel).normalized()
-
+	# 4. Set the velocity and rotation to the new direction
+	
+	# choose which traction value to use - at lower speeds, slip should be low
 	var traction = traction_slow
 	if velocity.length() > slip_speed:
 		traction = traction_fast
 	var d = new_heading.dot(velocity.normalized())
 	if d > 0:
-		velocity = velocity.lerp(new_heading * velocity.length(), traction)
-
+		velocity = lerp(velocity, new_heading * velocity.length(), traction * delta)
 	if d < 0:
-		velocity = -new_heading * min(velocity.length(), max_speed_reversed)
+		velocity = -new_heading * velocity.length()
 	rotation = new_heading.angle()
-
-func apply_friction():
-	if velocity.length() < 5:
-		velocity = Vector2.ZERO
-	var friction_force = velocity * friction
-	var drag_force = velocity * velocity.length() * drag
-	acceleration += drag_force + friction_force
-	
-
