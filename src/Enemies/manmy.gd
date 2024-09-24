172958c35391618ff9e23873b57037750c21f836
dinesh1160 <dinesharunachalam116@gmail.com>
2024-09-24 23:36:11 +0530

    updated enemy ai

--- src/Enemies/manmy.gd
+++ src/Enemies/manmy.gd
@@ -2,24 +2,26 @@ extends CharacterBody2D
 
 class_name Enemy
 
+@onready var attack_timer = $Attack_timer
+
 signal enemy_fired_bullet(bullet, position,direction)
 @export var Bullet : PackedScene
 #@export var Cars : PackedScene
 @onready var endpoint = $endpoint
-
 var car:Car = null   #car injuction
 
 var health = 5
 var engage = false
+var can_attack = true
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
 	if engage and car!=null:
-		print(car.global_position)
 		rotation = global_position.direction_to(car.global_position).angle()
-		shoot()
+		if can_attack:
+			shoot()
 
 func handle_hit():
 	health -= 1
@@ -33,10 +35,11 @@ func _on_area_2d_body_entered(body):
 		handle_hit()
 
 func shoot():
+	can_attack = false
 	var bullet_instance = Bullet.instantiate()
 	var direction = endpoint.global_position.direction_to(car.global_position).normalized() #finds the direction using another marker2d called target
 	emit_signal("enemy_fired_bullet",bullet_instance,endpoint.global_position,direction)
-	
+	attack_timer.start()
 
 func _on_player_dectection_body_entered(body): #to detect the car in range
 	if body.is_in_group("car"):
@@ -48,3 +51,6 @@ func _on_player_dectection_body_exited(body): #to detect the car out of range
 	if body.is_in_group("car"):
 		engage = false
 		car = null
+
+func _on_attack_timer_timeout():
+	can_attack = true
